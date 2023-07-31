b82ad672dc0c18e59acef05a2cb1749874a1b8f5
Ruzel <ruzeldenf222@gmail.com>
2023-07-31 22:54:17 +0300

    first commit

---
+++ src/pages/AddPost/index.jsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import SimpleMDE from 'react-simplemde-editor';
+
+import 'easymde/dist/easymde.min.css';
+import styles from './AddPost.module.scss';
+import { selectIsAuth } from '../../redux/slices/authSlice';
+import { useSelector } from 'react-redux';
+import { useNavigate, Navigate, useParams } from 'react-router-dom';
+import axios from '../../axios';
+
+export const AddPost = () => {
+  const isAuth = useSelector(selectIsAuth);
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = React.useState(false);
+  const [text, setText] = React.useState('');
+  const [title, setTitle] = React.useState('');
+  const [tags, setTags] = React.useState('');
+  const [imageUrl, setImageUrl] = React.useState('');
+  const inputfileRef = React.useRef(null);
+
+  const isEditing = Boolean(id);
+
+  const onClickRemoveImage = () => {
+    setImageUrl('');
+  };
+
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append('image', file);
+      const { data } = await axios.post('/upload', formData);
+      setImageUrl(data.url);
+    } catch (error) {
+      console.warn(error);
+      alert('Не удалось загрузить файл');
+    }
+  };
+
+  const onChange = React.useCallback((text) => {
+    setText(text);
+  }, []);
+
+  React.useEffect(() => {
+    if (id) {
+      axios
+        .get(`/posts/${id}`)
+        .then(({ data }) => {
+          setTitle(data.title);
+          setText(data.text);
+          setImageUrl(data.imageUrl);
+          setTags(data.tags);
+        })
+        .catch((err) => {
+          console.warn(err);
+          alert('Ошика при получении статьи');
+        });
+    }
+  }, []);
+
+  const options = React.useMemo(
+    () => ({
+      spellChecker: false,
+      maxHeight: '400px',
+      autofocus: true,
+      placeholder: 'Введите текст...',
+      status: false,
+      autosave: {
+        enabled: true,
+        delay: 1000,
+      },
+    }),
+    [],
+  );
+
+  const onSubmit = async () => {
+    try {
+      setLoading(true);
+      const { data } = isEditing
+        ? await axios.patch(`/posts/${id}`, { title, tags, text, imageUrl })
+        : await axios.post('/posts', { title, tags, text, imageUrl });
+
+      const _id = isEditing ? id : data._id;
+
+      navigate(`/posts/${_id}`);
+    } catch (err) {
+      console.warn(err);
+      alert('Ошибка при создании статьи!');
+    }
+  };
+
+  if (!window.localStorage.getItem('token') && !isAuth) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <Paper style={{ padding: 30 }}>
+      <Button onClick={() => inputfileRef.current.click()} variant="outlined" size="large">
+        Загрузить превью
+      </Button>
+      <input ref={inputfileRef} type="file" onChange={handleChangeFile} hidden />
+      {imageUrl && (
+        <>
+          <Button variant="contained" color="error" onClick={onClickRemoveImage}>
+            Удалить
+          </Button>
+          <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        </>
+      )}
+      <br />
+      <br />
+      <TextField
+        classes={{ root: styles.title }}
+        variant="standard"
+        placeholder="Заголовок статьи..."
+        fullWidth
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <TextField
+        classes={{ root: styles.tags }}
+        variant="standard"
+        placeholder="Тэги"
+        fullWidth
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
+      />
+      <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
+      <div className={styles.buttons}>
+        <Button onClick={onSubmit} size="large" variant="contained">
+          {isEditing ? 'Сохранить' : 'Опубликовать'}
+        </Button>
+        <a href="/">
+          <Button size="large">Отмена</Button>
+        </a>
+      </div>
+    </Paper>
+  );
+};
